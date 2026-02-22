78f6c84d9e674922443adb0adc1e7a9d903c5c8e
חיה וילמן <chailyvilman@MacBook-Air-sl-hyh.local>
2026-02-22 20:21:08 +0200

    update Project enty

--- WebApplication/WebApplication/Program.cs
+++ WebApplication/WebApplication/Program.cs
@@ -1,12 +1,33 @@
-var builder = WebApplication.CreateBuilder(args);
+global using Microsoft.AspNetCore.Builder;using Repository.Entities;
+using Repository.Interfaces;
+using Repository.Repositories;
+using Service.Dto;
+using Service.Interface;
+using Service.Services;
+using Service;
 
+var builder = Microsoft.AspNetCore.Builder.WebApplication.CreateBuilder(args);
+
+// Repositories
+builder.Services.AddScoped<IRepository<User>, UserRepository>();
+builder.Services.AddScoped<IRepository<Project>, ProjectRepository>();
+builder.Services.AddScoped<IRepository<TaskItem>, TaskItemRepository>();
+builder.Services.AddScoped<IRepository<SubTask>, SubTaskRepository>();
+builder.Services.AddScoped<IRepository<History>, HistoryRepository>();
 // Add services to the container.
 
+builder.Services.AddScoped<IService<UserDto>, UserService>();
+builder.Services.AddScoped<IService<ProjectDto>, ProjectService>();
+builder.Services.AddScoped<IService<TaskItemDto>, TaskService>();
+builder.Services.AddScoped<IService<SubTaskDto>, SubTaskService>();
+builder.Services.AddScoped<IService<HistoryDto>, HistoryService>();
 builder.Services.AddControllers();
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddAutoMapper(typeof(MyMapper).Assembly);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
