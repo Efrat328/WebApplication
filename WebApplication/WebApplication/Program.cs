4c21167c7d5f08ff9c3d7516a93802d090f333c0
חיה וילמן <chailyvilman@MacBook-Air-sl-hyh.local>
2026-02-22 20:24:28 +0200

    update Project enty

--- WebApplication/WebApplication/Program.cs
+++ WebApplication/WebApplication/Program.cs
@@ -27,7 +27,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddAutoMapper(typeof(MyMapper).Assembly);
+builder.Services.AddAutoMapper(cfg => cfg.AddProfile<MyMapper>());
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
