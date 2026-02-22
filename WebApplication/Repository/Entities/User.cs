e303cfa40748d42ab32cf10a9b1ccc492a8c9e43
user1 <user1@DESKTOP-1VUANBN>
2026-02-22 14:25:05 +0200

    pushAdd project files.

---
+++ WebApplication/Repository/Entities/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string NameUser { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public bool IsActive { get; set; }
+
+        // קשרים
+        public ICollection<Project> Projects { get; set; }
+        public ICollection<Task> Tasks { get; set; }
+    }
+}
