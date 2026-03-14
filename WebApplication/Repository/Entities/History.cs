0a08fd5fce80666054a72b0578a21a7d9df30060
חיה וילמן <chailyvilman@MacBook-Air-sl-hyh.local>
2026-03-15 00:20:30 +0200

    תיאור השינוי

--- WebApplication/Repository/Entities/History.cs
+++ WebApplication/Repository/Entities/History.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
 
 namespace Repository.Entities
 {
@@ -16,11 +18,15 @@ namespace Repository.Entities
     public class History
     {
         public int Id { get; set; }
+        [Required]
         public int SubTaskId { get; set; }
+        [Required]
         public HistoryStatus OldStatus { get; set; }
-        public HistoryStatus NewStatus { get; set; } 
+        [Required]
+        public HistoryStatus NewStatus { get; set; }
+        [Required] 
         public DateTime ChangedAt { get; set; }
-        public bool IsActive { get; set; }
+        public bool IsActive { get; set; }= true;
 
         // connections
         public SubTask SubTask { get; set; }
